e8e6c2e2fd3eef094d5d6b92c2f7f4e3b50086fe
Vishal Jain2 <vishal_jain2@epam.com>
2025-11-08 12:00:02 +0530

    Use postgres image for docker

--- src/main/java/com/springboot/Application.java
+++ src/main/java/com/springboot/Application.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-@RestController
 public class Application {
 
     public static void main(String[] args) {
